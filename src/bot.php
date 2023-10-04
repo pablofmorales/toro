5fca8ee98f3bb51ee2c2b4d78bf44448f3d758ea
Pablo Morales <pablofmorales@gmail.com>
2023-10-04 14:13:33 -0300

    Update toro capabilties

--- src/bot.php
+++ src/bot.php
@@ -25,7 +25,39 @@ $discord->on('ready', function (Discord $discord) {
         if (strtolower($message->content) === '!ip') {
             $message->reply('45.235.98.248:27215');
         }
-        echo "{$message->author->username}: {$message->content}", PHP_EOL;
+
+        if (strtolower($message->content) === '!ricos' ) {
+            $steamWebApi = new \SteamWebApi\SteamWebApi('YP4ACBJB0CS4KS56');
+
+            $economics = json_decode(file_get_contents('data/Economics.json'), true);
+
+            foreach ($economics['Balances'] as $id => $value) {
+                preg_match('/\d{17}/', $id, $matches, PREG_OFFSET_CAPTURE);
+                if (isset($matches[0][0])) {
+                    $steamId = $matches[0][0];
+                    $profile = $steamWebApi->getProfile('', $steamId);
+                
+                    echo $profile['personaname'];
+                    echo "Balance: $" . $value. PHP_EOL;
+                }
+            }
+        }
+
+        if (strtolower($message->content) === '!top' ) {
+            
+            $top = json_decode(file_get_contents('https://toro.pablomorales.me/api/players/top'), true);
+
+            $counter = 1;
+            $result = 'Top 15 jugadores ' . PHP_EOL;
+            foreach ($top as $score) {
+                $result .= $counter . "- " . $score['name'] . " Score: " . $score['score'] . PHP_EOL;
+
+                $counter ++;
+            }
+            $message->reply($result);
+        }
+
+        
         // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
     });
 });
